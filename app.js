501720fc11c11d9119a3bc90d2e452fd93209338
Lizdigital <lizetteclaireh@gmail.com>
2024-05-06 20:57:00 +0100

    Add time variables

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 
 const playBtn = document.querySelector('#playbtn');
 const resetBtn = document.querySelector('#resetbtn');
+
+// Variables to hold time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
